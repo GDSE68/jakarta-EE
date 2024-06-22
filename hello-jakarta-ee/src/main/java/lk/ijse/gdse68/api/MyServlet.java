5a695fbafa52c8fdc270dad2965eec5b0a1d634f
Pebhashi Tharushika <pebhashitharushika@gmail.com>
2024-06-22 21:39:36 +0530

    create 2 servlet in same app

---
+++ hello-jakarta-ee/src/main/java/lk/ijse/gdse68/api/MyServlet.java
@@ -0,0 +1,16 @@
+package lk.ijse.gdse68.api;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+public class MyServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.getWriter().println("Hello, I'm from MyServlet");
+
+    }
+}
