b5e31ff4bc1b3550ed3314f62925df78e5ff7bee
Pebhashi Tharushika <pebhashitharushika@gmail.com>
2024-06-22 20:33:20 +0530

    reate first jakarata EE project

---
+++ hello-jakarta-ee/src/main/java/lk/ijse/gdse68/api/HelloServlet.java
@@ -0,0 +1,16 @@
+package lk.ijse.gdse68.api;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+public class HelloServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("incoming request: "+req.getRemoteAddr());
+        resp.getWriter().println("Hello, I'm from HelloServlet");
+    }
+}
