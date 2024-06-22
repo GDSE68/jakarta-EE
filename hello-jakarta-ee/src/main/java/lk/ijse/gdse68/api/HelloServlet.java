8282c04a13de641919afba125962d14569e8d9a9
Pebhashi Tharushika <pebhashitharushika@gmail.com>
2024-06-22 20:51:00 +0530

    write simple html code in response

--- hello-jakarta-ee/src/main/java/lk/ijse/gdse68/api/HelloServlet.java
+++ hello-jakarta-ee/src/main/java/lk/ijse/gdse68/api/HelloServlet.java
@@ -11,6 +11,6 @@ public class HelloServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         System.out.println("incoming request: "+req.getRemoteAddr());
-        resp.getWriter().println("Hello, I'm from HelloServlet");
+        resp.getWriter().println("<h1 style='color: red'>Hello, I'm from HelloServlet</h1>");
     }
 }
